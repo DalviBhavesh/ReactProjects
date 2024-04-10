51848e965179f0c37adebd4dc5fcb0a49efaae60
DalviBhavesh <dalvibhavesh007@gmail.com>
2024-04-11 03:12:30 +0530

    ui changes

--- ReactProject5/src/App.jsx
+++ ReactProject5/src/App.jsx
@@ -1,6 +1,7 @@
 
 
 import Login from './component/Login'
+import Nav from './component/Nav'
 import Profile from './component/Profile'
 import UserContextProvider from './context/UserContextProvider'
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <UserContextProvider>
       <div className='w-lvw h-lvh flex justify-center items-center flex-col'>
+      <Nav />
       <Login />
         
       </div>
--- ReactProject5/src/component/Nav.jsx
+++ ReactProject5/src/component/Nav.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 function Nav(){
     return(
         <>
-            
+            <nav className='fixed top-0 w-full flex justify-start items-center h-14 bg-black'>
+                <h1 className='text-white text-2xl font-bold px-3'>React <span className='text-green-400'>Context API</span> Practice</h1>
+            </nav>
         </>
     )
 }
