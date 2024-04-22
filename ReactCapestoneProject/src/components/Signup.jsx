2386a5dc1ef738dd04a7b2ca94c7774baab0e56d
DalviBhavesh <dalvibhavesh007@gmail.com>
2024-04-22 23:16:52 +0530

    completing project

--- ReactCapestoneProject/src/components/Signup.jsx
+++ ReactCapestoneProject/src/components/Signup.jsx
@@ -1,53 +1,35 @@
+import React,{useState} from 'react'
+import authService from '../appwrite/auth';
+import  {Link, useNavigate} from 'react-router-dom';
+import {login} from'../store/authSlice'
+import {Button, Input, Logo} from './index'
+import {useDispatch} from 'react-redux';
+import {useForm} from 'react-hook-form';
 
-// Learned to used react form and other good practices
-
-import React, {useState} from 'react';
-import {Link, useNavigate} from 'react-router-dom';
-import { login as authLogin} from '../store/authSlice'
-import {Button, Input, Logo} from "./index"
-import { useDispatch } from 'react-redux';
-import authService from "../appwrite/auth"
-import {useForm} from "react-router-dom"
-
-
-
-function Login(){
-
-
-
-     // useNavigate() is just an alternative of Link / NavLink attribute in react-router 
-     // the only difference is that:
-     // we are using navigate here because using navigate we  can 
-     //redirect user to desired page without the need of the user to actually click on the link 
-     //ie.(Programatically) 
+function Signup(){
 
     const navigate = useNavigate();
-    const dispatch = useDispatch();
-    const {register, handleSubmit} = useForm();
     const [error, setError] = useState("");
+    const dispatch = useDispatch();
+    const { register, handleSubmit } = useForm();
 
-    const login = async(data) => {
-        setError("")
+    const create = async(data)=>{
+        setError("");
         try{
-            const session = await authService.login(data);
-            if(session){
-                const userData = await authService.getCurrentUser();
-                
-                if(userData) dispatch(authLogin(userData));
-
-                navigate("/");
+            const userData = await authService.createAccount(data);
+            if(userData){
+               const userData =  await authService.getCurrentUser();
+               if(userData)( dispatch(login(userData)));
+               navigate("/");
             }
-        }
-        catch(error){
-            setError(error.message)
+        }catch(error){
+            setError(error.message);
         }
     }
- 
+
     return(
         <>
-            <div
-                className='flex items-center justify-center w-full'
-            >
+            <div className='flex items-center justify-center'>
                 <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
                     <div className='mb-2 flex justify-center'>
                         <span className='inline-block w-full max-w-[100px]'>
@@ -55,24 +37,33 @@ function Login(){
                         </span>
                     </div>
                     <h2 className='text-center text-2xl font-bold leading-tight'>
-                        Sign in to your account
+                        Sign up to create an account
                     </h2>
                     <p className='mt-2 text-center text-base text-black/60'>
-                        Don&apos;t have any account?&nbsp;
+                        Already have any account?&nbsp;
                         <Link 
                             to="/signup"
                             className='font-medium text-primary transition-all duration-200 hover:underline'
                         >
-                            Sign Up
+                            Sign In
                         </Link>
                     </p>
                     {
                         error && <p className='text-red-600 mt-8 text-center'>{error}</p>
                     }
-                    <form onSubmit={handleSubmit(login)} className='mt-8'>
-                        <div
-                            className='space-y-5' 
-                        >
+
+                    <form onSubmit={handleSubmit(create)}>
+                        <div className='space-y-5'>
+                            <Input
+                                label="Full Name: "
+                                placeholder="Enter your name"
+                                {
+                                    ...register("name",{
+                                        required: true,
+                                    })
+                                }
+                            />
+
                             <Input type="email"
                                 label="Email: "
                                 placeholder='Enter your email'
@@ -87,25 +78,29 @@ function Login(){
                             />
 
                             <Input
-                                label="password: "
-                                type="password"
-                                placeholder="Enter your password"
+                                label = "password"
+                                type = "password"
+                                placeholder = "Enter your password"
                                 {
                                     ...register("password",{
                                         required: true,
                                     })
                                 }
                             />
-                            <Button
+
+                            <Button 
                                 type = "submit"
-                                className = "w-full"
-                            >Sign in</Button>
+                                className="w-full"
+                            > 
+                                Create Account
+                            </Button>
                         </div>
                     </form>
                 </div>
+                
             </div>
         </>
     )
 }
 
-export default Login;
+export default Signup; 
